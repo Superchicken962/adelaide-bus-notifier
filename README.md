846f0ea898c69fa389b0c0fa99785b59021d3ef0
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2024-07-08 20:15:31 +0930

    Add screenshot from my vehicles website to embed

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # adelaide-bus-notifier
 Script to send discord webhook when specified vehicles are in service.
+Notifications are currently sent each time the vehicle is on a new trip, so if a vehicle goes onto a new trip afterwards then a new notification will be sent. This may change in a later version.
 
 ## How to run  
 1. Run `npm install` in project directory - this will install all the project dependencies.
 2. Add webhook values, and any vehicles you want to be notified about into `config.json`
 3. Start the script with `node index.js`
 
+## Configuring  
+In `config.json`, you can configure the following:
+- `discordId` - Your discord ID (only needed if you want it to ping you)
+- `pingOnVehicle` - Ping you when a vehicle is found?
+- `recheckIntervalSeconds` - How long to wait before checking again on completion.
+- `webhooks.vehicleAlert` - Where to send the vehicle message (Discord webhook URL).
+- `vehicles` - Array of vehicle IDs that you want to be notified for.
+
 ### Notes
-- The script may end at any time, so it is recommended to use either [pm2](https://pm2.keymetrics.io/), or a batch script to auto-restart if it does stop.
+- While the script has been set to stay online, it may still end from an unexpected error, so it may benefit you to use either [pm2](https://pm2.keymetrics.io/), or a batch script to auto-restart if it does stop.
+- Currently, running the script requires [Node.js](https://nodejs.org/en). Hopefully in the future, this will not be the case.
--- index.js
+++ index.js
@@ -43,16 +43,56 @@ function getVehicleType(route) {
     return type;
 }
 
+/**
+ * Scrapes apt.markgurney.dev and gets screenshot of vehicle page.
+ * @param { String } id - Vehicle id
+ * @param { String } type - Vehicle type (bus, train, tram)
+ */
+function getVehiclesPageScreenshot(id, type) {
+    return new Promise(async(resolve) => {
+
+    });
+}
+
 // Store vehicle trip ids, so that info is not sent multiple times for the same trip.
 const vehicleLastTrip = fs.existsSync(path.join(__dirname, "vehicles_data.json")) ? JSON.parse(fs.readFileSync(path.join(__dirname, "vehicles_data.json"), "utf-8")) : {};
 
 function checkVehicles() {
+    console.log("Checking vehicles for matches..."); 
     request("https://gtfs.adelaidemetro.com.au/v1/realtime/vehicle_positions", {
         "method": "GET",
         "encoding": null
     }, async(err, response, body) => {
         if (err || response.statusCode !== 200) {
             console.error("Unable to reach Adelaide Metro API!");
+
+            // Try again soon.
+            setTimeout(checkVehicles, (config.recheckIntervalSeconds*1000));
+            return;
+        }
+
+        const matchingVehicles = [];
+        // Parse config each time so vehicles can be updated without restarting script.
+        const vehiclesToCheckFor = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf-8")).vehicles;
+
+        const vehicles = ((await decodeProto(body)).entity).map(entity => entity.vehicle);
+        vehicles.forEach(vehicle => {
+            if (vehiclesToCheckFor.includes(vehicle.vehicle.id)) matchingVehicles.push(vehicle);
+        });
+
+        if (vehicles.length === 0) {
+            console.warn("No live vehicles! Checking again shortly.");
+
+            // Try again soon.
+            setTimeout(checkVehicles, (config.recheckIntervalSeconds*1000));
+            return;
+        }
+
+        if (matchingVehicles.length === 0) {
+            console.log("No matching vehicles found, Checking again shortly.");
+
+            // Try again soon.
+            setTimeout(checkVehicles, (config.recheckIntervalSeconds*1000));
             return;
         }
 
@@ -60,48 +100,97 @@ function checkVehicles() {
         const routesFile = readStaticFileContent("routes.txt");
         const tripsFile = readStaticFileContent("trips.txt");
 
-        const matchingVehicles = [];
-
-        const vehicles = ((await decodeProto(body)).entity).map(entity => entity.vehicle);
-        vehicles.forEach(vehicle => {
-            if (config.vehicles.includes(vehicle.vehicle.id)) matchingVehicles.push(vehicle);
+        // Launch browser to be navigated in for loop.
+        const browser = await puppeteer.launch({
+            "headless": "new",
+            "defaultViewport": null
         });
+
+        const page = await browser.newPage();
+
+        let vehiclesChanged = 0;
 
         // Send discord webhook notifications if trip id is different to last check.
         for (const vehicle of matchingVehicles) {
+            console.log(`Checking vehicle ${vehicle.vehicle.id}`);
+
             // Skip vehicle if on same trip as last check.
             if (vehicleLastTrip[vehicle.vehicle.id] === vehicle.trip.tripId) continue;
 
             // Assign the new trip's id to vehicle.
             vehicleLastTrip[vehicle.vehicle.id] = vehicle.trip.tripId;
 
-            const routeInfo = findInFile.routeInfoByRoute(vehicle.trip.routeId);
-            const tripInfo = findInFile.tripInfoByTripId(vehicle.trip.tripId);
+            const routeInfo = findInFile.routeInfoByRoute(vehicle.trip.routeId, routesFile);
+            const tripInfo = findInFile.tripInfoByTripId(vehicle.trip.tripId, tripsFile);
+    
+            const url = `https://apt.markgurney.dev/vehicle/${getVehicleType(vehicle.trip.routeId)}/${vehicle.vehicle.id}`;
+            await page.goto(url, {
+                "waitUntil": "networkidle2",
+                "timeout": 30000
+            });
 
-            console.log(routeInfo);
-            console.log(tripInfo);
+            // This element should be present on the page when the data is loaded.
+            await page.waitForSelector("br.s[data-is-ready]");
 
-            console.log(routeInfo.route_color);
+            // If the screenshots folder does not exist, make it.
+            if (!fs.existsSync("vehicle_screenshots/")) {
+                fs.mkdirSync("vehicle_screenshots/");
+            }
 
-            // Send to Discord webhook.
-            axios.post(config.webhooks.vehicleAlert, {
+            // Screenshot page, and save as vehicle id.
+            const imageFileName = `${vehicle.vehicle.id}.png`;
+            await page.screenshot({"path": "vehicle_screenshots/"+imageFileName});
+
+            // Use formData to send image to webhook with multipart header.
+            // https://stackoverflow.com/questions/60629800/nodejs-handle-and-send-multipart-request
+            const form = new FormData();
+
+            // Add file to form data.
+            form.append("files[0]", new Blob([fs.readFileSync(path.join(__dirname, "vehicle_screenshots/"+imageFileName))]), imageFileName);
+
+            const webhookData = {
                 "username": vehicle.vehicle.id,
                 "embeds": [{
                     "title": `${routeInfo.route_short_name} - ${tripInfo.trip_headsign}`,
                     "color": Number(`0x${routeInfo.route_color}`), // Convert hex to int
                     "fields": [
                         {"name": routeInfo.route_long_name, "value": `[View info](https://apt.markgurney.dev/vehicle/${getVehicleType(vehicle.trip.routeId)}/${vehicle.vehicle.id})`}
-                    ]
+                    ],
+                    "image": {
+                        "url": `attachment://${imageFileName}`
+                    },
+                    "timestamp": new Date(vehicle.timestamp.low*1000).toISOString()
                 }]
+            };
+
+            // If a discord id is provided, and pinging on vehicle is enabled, then add the user mention to the webhook content.
+            if (config.discordId && config.pingOnVehicle) webhookData.content = `<@!${config.discordId}>`;
+
+            // Add webhook json to form data.
+            form.append("payload_json", JSON.stringify(webhookData));
+
+            // Finally send the form data to webhook.
+            axios.post(config.webhooks.vehicleAlert, form, {
+                "headers": {"Content-Type": `multipart/form-data; boundary=${form._boundary}`}
             });
+
+            vehiclesChanged++;
         }
 
-        console.log(vehicleLastTrip);
+        // Close page & browser since we're done with them now.
+        await page.close();
+        await browser.close();
 
         // Save data to file, so it can be loaded if script is restarted - prevents resending same trip if already sent.
         fs.writeFileSync(path.join(__dirname, "vehicles_data.json"), JSON.stringify(vehicleLastTrip), "utf-8");
+
+        console.clear();
+        console.log(`Update complete - found ${matchingVehicles.length} vehicle(s). Updated ${vehiclesChanged}.`);
+        console.log(`Updating again in ${config.recheckIntervalSeconds} seconds.`);
+
+        // Call function again after recheck interval seconds has passed.
+        setTimeout(checkVehicles, (config.recheckIntervalSeconds*1000));
     });
 }
 
-setInterval(checkVehicles, 120000);
 checkVehicles();
